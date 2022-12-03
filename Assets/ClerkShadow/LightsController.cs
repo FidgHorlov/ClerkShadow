8688aa7b86bd98bee2063c347d0034914c4e1d6d
Andrii Horlov <ahorlov@tsukat.com>
2022-12-03 21:55:43 +0000

    Delete unusing code

--- Assets/ClerkShadow/LightsController.cs
+++ Assets/ClerkShadow/LightsController.cs
@@ -11,6 +11,7 @@ namespace ClerkShadow
         [SerializeField] private Color _endColor; 
         [Space]
         [SerializeField] private Transform _followTarget;
+        
         private Color _defaultColor;
         private float _minimalLightIntensity;
         private Vector3 _defaultLightPos;
@@ -44,33 +45,5 @@ namespace ClerkShadow
             _light.color = _defaultColor;
             _currentTransform.localPosition = _defaultLightPos;
         }
-    
-#region  Obsolete
-//    [SerializeField] private float _intensityGrowthSpeed = 5f;
-//    [SerializeField] private Color _startColor;
-//    private float _currentLightIntensity;
-//    private void Update()
-//    {
-//        if (_currentLightIntensity >= _maxLightIntensity)
-//        {
-//            return;
-//        }
-//
-//        UpdateLightIntensity();
-//        UpdateLightColor();
-//    }
-//
-//    private void UpdateLightIntensity()
-//    {
-//        _currentLightIntensity += Time.deltaTime * _intensityGrowthSpeed * 0.1f;
-//        _light.intensity = _currentLightIntensity;
-//    }
-//
-//    private void UpdateLightColor()
-//    {
-//        _light.color = Color.Lerp(_startColor, _endColor,
-//            Mathf.InverseLerp(_minimalLightIntensity, _maxLightIntensity, _currentLightIntensity));
-//    }
-#endregion
     }
 }
