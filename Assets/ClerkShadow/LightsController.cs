b52551fa633473af142534c0046142795e809c7f
Andrii Horlov <ahorlov@tsukat.com>
2022-12-04 14:37:11 +0000

    Change Lights adjustement to the Sprite ones

--- Assets/ClerkShadow/LightsController.cs
+++ Assets/ClerkShadow/LightsController.cs
@@ -1,49 +1,51 @@
 ﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ClerkShadow
 {
     public class LightsController : MonoBehaviour
     {
-        [SerializeField] private Light _light;
-        [SerializeField] private float _targetLightPosY;
-        [SerializeField] private float _maxLightIntensity;
+        private const float MaximumSize = 5f;
+        
+        [SerializeField] private Image _spotImage;
         [SerializeField] private Color _endColor; 
         [Space]
         [SerializeField] private Transform _followTarget;
         
         private Color _defaultColor;
-        private float _minimalLightIntensity;
         private Vector3 _defaultLightPos;
-        private Transform _currentTransform;
+        private Transform _spotTransform;
 
         private void Start()
         {
-            _minimalLightIntensity = _light.intensity;
-            _defaultColor = _light.color;
-            _currentTransform = transform;
-            _defaultLightPos = _currentTransform.localPosition;
+            _defaultColor = _spotImage.color;
+            _spotTransform = _spotImage.transform;
+            _defaultLightPos = _spotTransform.localPosition;
         }
 
         private void Update()
         {
-            _currentTransform.position = _followTarget.position;
+            _spotTransform.position = _followTarget.position;
         }
 
         public void StartLighting(float duration)
         {
-            _light.DOColor(_endColor, duration);
-            _light.DOIntensity(_maxLightIntensity, duration).OnComplete(() => _light.DOKill());
-            _currentTransform.DOMoveY(_targetLightPosY, duration).OnComplete(() => _currentTransform.DOKill());
+            _spotImage.DOKill(_spotImage.sprite);
+            _spotImage.DOColor(_endColor, duration).SetId(_spotImage.sprite);
+
+            _spotTransform.DOKill(_spotTransform);
+            _spotTransform.DOScale(MaximumSize, duration).SetId(_spotTransform);
         }
 
         public void ResetLights()
         {
-            _light.DOKill();
-            _currentTransform.DOKill();
-            _light.intensity = _minimalLightIntensity;
-            _light.color = _defaultColor;
-            _currentTransform.localPosition = _defaultLightPos;
+            _spotImage.DOKill(_spotImage.sprite);
+            _spotTransform.DOKill(_spotTransform);
+            _spotTransform.DOKill(_spotTransform.transform);
+            
+            _spotImage.color = _defaultColor;
+            _spotTransform.localPosition = _defaultLightPos;
         }
     }
 }
