e1f59cd84d37af38c9623cfff2516dc50773f5bf
Andrii Horlov <ahorlov@tsukat.com>
2022-12-09 08:37:05 +0000

    Character movement

--- Assets/ClerkShadow/Character.cs
+++ Assets/ClerkShadow/Character.cs
@@ -1,4 +1,5 @@
-﻿using ClerkShadow.Data;
+﻿using System;
+using ClerkShadow.Data;
 using DG.Tweening;
 using UnityEngine;
 
@@ -9,31 +10,35 @@ namespace ClerkShadow
         [SerializeField] private AnimationManager _animationManager;
         [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private float _speed;
-        [Header("Resizing")] 
-        [SerializeField] private float _targetSize;
+        [Header("Resizing")] [SerializeField] private float _targetSize;
         [SerializeField] private float _jumpForce;
-    
+
 #region Private fields
+
         private Transform _currentTransform;
         private Vector3 _defaultFlippedSize;
         private Vector3 _targetSizeVector;
         private Vector3 _currentSize;
         private Vector3 _defaultSize;
         private Vector3 _defaultPosition;
-    
+
         private float _currentDuration; // keep the duration for resizing
+        private float _resizeDurationLeft;
         private float _timeCounter;
         private bool _isLeftLooking;
-    
+
         private bool _isJumpAllowed;
         private bool _isScaling;
+
 #endregion
-    
+
         public bool IsRunning { get; private set; }
         public bool IsLevelReset { get; set; }
+        public bool IsGameStarted { get; set; }
 
 #region Monobehaviour
-        protected  void Awake()
+
+        protected void Awake()
         {
             _currentTransform = transform;
             _defaultPosition = _currentTransform.position;
@@ -44,8 +49,14 @@ namespace ClerkShadow
             _defaultFlippedSize = _defaultSize;
             _defaultFlippedSize.x *= -1f;
         }
+
         private void FixedUpdate()
         {
+            if (!IsGameStarted)
+            {
+                return;
+            }
+
             Movement();
         }
 
@@ -71,12 +82,12 @@ namespace ClerkShadow
         {
             return CheckTheTag(other, Constants.Tags.Ground) || CheckTheTag(other, Constants.Tags.Obstacle);
         }
-        
+
         private bool CheckTheTag(Collision2D targetCollision, string targetTag)
         {
             return targetCollision.gameObject.CompareTag(targetTag);
         }
-    
+
 #region Pose changing
 
         private void Movement()
@@ -98,7 +109,7 @@ namespace ClerkShadow
                     Flip();
                 }
             }
-        
+
             _rigidbody2D.velocity = new Vector2(horizontal * _speed, _rigidbody2D.velocity.y);
             IsRunning = horizontal != 0;
             _animationManager.SetBool(Constants.AnimationState.Run, IsRunning);
@@ -117,13 +128,11 @@ namespace ClerkShadow
             _currentSize.x *= -1f;
             _currentTransform.localScale = _currentSize;
             _targetSizeVector.x *= -1f;
+            if (!_isScaling) return;
 
-            if (_isScaling)
-            {
-                _isScaling = false;
-                _currentTransform.DOKill();
-                StartResize();
-            }
+            _currentTransform.DOKill();
+            _isScaling = false;
+            StartResize();
         }
 
         public void ResetPlayer()
@@ -135,23 +144,35 @@ namespace ClerkShadow
             _currentTransform.position = _defaultPosition;
         }
 
+        private TimeSpan _startResizing;
+
         public void StartResize(float duration = -1f)
         {
             if (duration > 0)
             {
-                _currentDuration = duration;   
+                _currentDuration = duration;
+                _resizeDurationLeft = _currentDuration;
+                _startResizing = DateTime.Now.TimeOfDay;
             }
-        
+            else
+            {
+                _resizeDurationLeft = _currentDuration - (DateTime.Now.TimeOfDay - _startResizing).Seconds;
+            }
+
             _isScaling = true;
-            _currentTransform.DOScale(_targetSizeVector, _currentDuration)
+            _currentTransform
+                .DOScale(_targetSizeVector, _resizeDurationLeft)
+                .SetEase(Constants.DoTweenDefaultEase)
+                .OnUpdate(() =>
+                {
+                    _currentSize = _currentTransform.localScale;
+                })
                 .OnComplete(() =>
                 {
                     HideShadow();
                     _isScaling = false;
                     _currentTransform.DOKill();
-                })
-                .OnUpdate(() => { _currentSize = _currentTransform.localScale; })
-                ;
+                });
         }
 
         private void HideShadow()
